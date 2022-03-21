9109a0a3f8f83edf719f88819976007e9cdadfff
Andrew <a.oleyn1kov@outlook.com>
2022-03-21 21:38:12 +0300

    Updates

--- precache-manifest.1bc43fe88c013fe7e3bdd5d1c1b16308.js
+++ precache-manifest.1bc43fe88c013fe7e3bdd5d1c1b16308.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4ea3661a861118e8e92f41d57fbff457",
+    "revision": "f3e5e49cb3c99b3a4f606fe0248101e9",
     "url": "/WebAutomataConstructor/index.html"
   },
   {
-    "revision": "67e7b3cce82ef7a78bf0",
+    "revision": "2a152fc4d245ac09361a",
     "url": "/WebAutomataConstructor/static/css/main.5b380ef1.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/WebAutomataConstructor/static/js/2.c34f458d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "67e7b3cce82ef7a78bf0",
-    "url": "/WebAutomataConstructor/static/js/main.d6c3536d.chunk.js"
+    "revision": "2a152fc4d245ac09361a",
+    "url": "/WebAutomataConstructor/static/js/main.bc0148fb.chunk.js"
   },
   {
     "revision": "5e16b6494e7be1c8c6f1",
