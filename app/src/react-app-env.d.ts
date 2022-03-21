78f8b993f8f6847c0a8ae694ec20e59d21926711
Andrew <a.oleyn1kov@outlook.com>
2022-03-21 21:27:49 +0300

    Updates

--- app/src/react-app-env.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react-scripts" />
-declare module "react-graph-vis";
-declare module 'lodash';
-declare module "vis";
---
+++ precache-manifest.887190f199197eab28a540c5906107dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea3661a861118e8e92f41d57fbff457",
+    "url": "/WebAutomataConstructor/index.html"
+  },
+  {
+    "revision": "67e7b3cce82ef7a78bf0",
+    "url": "/WebAutomataConstructor/static/css/main.5b380ef1.chunk.css"
+  },
+  {
+    "revision": "25edb0bb2cd20b98bf1a",
+    "url": "/WebAutomataConstructor/static/js/2.c34f458d.chunk.js"
+  },
+  {
+    "revision": "8fdd42fce7693954cfd79046877d932e",
+    "url": "/WebAutomataConstructor/static/js/2.c34f458d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67e7b3cce82ef7a78bf0",
+    "url": "/WebAutomataConstructor/static/js/main.d6c3536d.chunk.js"
+  },
+  {
+    "revision": "5e16b6494e7be1c8c6f1",
+    "url": "/WebAutomataConstructor/static/js/runtime-main.d935f7de.js"
+  }
+]);
